3ee27478ec3a129fc618364ab68ebcda4892e36a
josemeroq <JAMQ1316762374@gmail.com>
2022-05-18 20:34:51 -0500

    Agregar archivos de proyecto.

---
+++ Leer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JoseMero3AEJ6
+{
+
+    public class Leer
+    {
+        private int num { set; get; }
+        private int[] numv { set; get; }
+        private int suma { get; set; }
+        public Leer()
+        {
+            this.num = 51;
+        }
+
+        public void numerohasta()
+        {
+            /* do
+             {
+
+                 Console.WriteLine(--num);
+
+             } while (num != 0); */
+
+            if (num < 1)
+            {
+                do
+                {
+
+                    Console.WriteLine(++num);
+
+                } while (num != 0);
+
+                numv = new int[num];
+                for (int i = 0; i <= num; i++)
+                {
+
+                    Console.WriteLine("Numeros " + numv[i]);
+                    suma = suma + numv[i];
+                }
+                Console.WriteLine("Suma total: " + suma);
+            }
+            else
+            {
+                do
+                {
+
+                    Console.WriteLine(--num);
+
+                } while (num != 0);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace JoseMero3AEJ6
+{
+    class Program
+    {
+            static void Main(string[] args)
+            {
+                Leer numeroh = new Leer();
+                numeroh.numerohasta();
+            }
+        
+    }
+}
